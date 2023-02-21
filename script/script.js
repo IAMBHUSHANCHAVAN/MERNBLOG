cd1ab22468f69d3998b2849eacda84573429f632
Bhushan <bhushanchavan976@gmail.com>
2023-02-21 20:32:46 +0530

    formlogin

--- script/script.js
+++ script/script.js
@@ -9,3 +9,18 @@ function myFunction() {
     document.getElementById("h2").innerHTML = z;
     alert(`you read our blog - ${x} \n your most favourate blog is ${y} \n and your feed back is ${z}` )
   }
+
+  function fnc(){
+    let passs = document.getElementById("pass").value
+    let confpass = document.getElementById("conpass").value
+
+    if (passs!=confpass){
+      alert("enter same password")
+    }
+    else if(passs.length>8){
+      alert("too long")
+    }
+    else{
+      alert("matched")
+    }
+  }
